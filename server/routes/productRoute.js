653246f89cab64bb0911c37956b74fb0e86f8c05
vartika <dhochakvartika@gmail.com>
2026-07-16 01:17:26 +0530

    Initial commit

---
+++ server/routes/productRoute.js
@@ -0,0 +1,20 @@
+import express from "express";
+import { upload } from "../configs/multer.js";
+import authSeller from "../middlewares/authSeller.js";
+import {
+  addProduct,
+  changeStock,
+  deleteProduct,
+  productById,
+  productList,
+} from "../controllers/productController.js";
+
+const productRouter = express.Router();
+
+productRouter.post("/add", upload.array(["images"]), authSeller, addProduct);
+productRouter.get("/list", productList);
+productRouter.get("/id", productById);
+productRouter.post("/stock", authSeller, changeStock);
+productRouter.post("/delete", authSeller, deleteProduct);
+
+export default productRouter;
